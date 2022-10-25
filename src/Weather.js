2d72e68eefb0ae3ec8d848854639e0e9f2413b30
coolanjali <112107778+coolanjali@users.noreply.github.com>
2022-10-25 14:10:38 +0200

    changes property from weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,29 +1,29 @@
 import React from "react";
-import axios from "axios";
 import "./Weather.css";
-export default function Weather() {
-  let Form = (
-    <form onSubmit={changeForm}>
-      <input type="search" onChange={changeCity} />
-      <input type="submit" value="Search" />
-    </form>
-  );
-  if (loaded) {
-    return (
-      <div>
-        {Form}
-        <ul className="showMe">
-          <li>Temperature :{Math.round(temperature)} °C</li>
-          <li>Wind : {Math.round(wind)} km/h</li>
-          <li>Description :{description}</li>
-          <li>Humidity :{Math.round(humidity)} %</li>
-          <li>
-            <img src={icon.Icon} alt={setDescription.description} />
-          </li>
-        </ul>
-      </div>
+export default function Weather(){
+    return(
+        <div className="container">
+            <form>
+                <input type="search" placeholder="Enter city name"/>
+                <input type="submit" placeholder="Search" className="btn btn-primary"/>
+            </form>
+          <h2>Paris</h2>
+         <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" classname="picture"></img>
+         <div class="container text-center">
+  <div class="row">
+    <div class="col order-last">
+      First in DOM, ordered last
+    </div>
+    <div class="col">
+      Second in DOM, unordered
+    </div>
+    <div class="col order-first">
+      Third in DOM, ordered first
+    </div>
+  </div>
+</div>
+            
+             </div>
+
     );
-  } else {
-    return Form;
-  }
 }
